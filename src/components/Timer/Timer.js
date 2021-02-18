a0a3d6b4931570c7645267892c9923975a612b39
rapioanfernee <rapioanfernee@gmail.com>
2021-02-18 15:37:27 +0800

    Change text

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Timer = ({ timeRemaining }) => {
     return (
-        <div>{timeRemaining}</div>
+        <div>Time Remaining: {timeRemaining} sec/s</div>
     )
 }
 
